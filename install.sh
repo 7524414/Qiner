7174ebe8d05f521a8fe93650acea35bd573c25cc
forci0ne <peter.deboer@outlook.com>
2023-01-04 20:24:09 +0000

    automated running with additional programsAdd files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+u="$USER"
+sudo apt update
+yes | sudo apt install openssh-server
+sudo ufw allow ssh
+yes | sudo apt install git
+sudo git clone https://www.github.com/forci0ne/Qiner.git /home/$u/Qiner
+yes | sudo apt install screen
+yes | sudo apt install htop
+yes | sudo apt install net-tools
+yes | sudo apt install g++
+sleep 2
+/usr/bin/g++ -Wall -march=native -Ofast -funroll-loops -pthread /home/$u/Qiner/QinerLinux.cpp -o /home/$u/Qiner/Qiner
+sleep 2
+cd /home/$u/Qiner
+sudo chmod -R 777 Qiner
+./Qiner 141.94.135.157 30
