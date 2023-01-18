9a89465448ed5b8fe0e6c2b3106229dd00796fc5
Peter <peter.deboer@outlook.com>
2023-01-18 17:19:18 +0000

    Updated
    1.83.0

--- QinerLinux.cpp
+++ QinerLinux.cpp
@@ -1,4 +1,4 @@
-/*
+/* 
 The software is provided "as is," without warranty of any kind, express or implied, 
 including but not limited to the warranties of merchantability, 
 fitness for a particular purpose and noninfringement. 
